4689e11706746dd34063d5ce7b80b4364a63d4b0
berfail <kunsangaboberfy@gmail.com>
2022-08-22 21:41:05 +0200

    :construction:

---
+++ spec/features/creating_projects_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+RSpec.feature "Users can create new project" do
+  scenario "with valid attributes" do
+    visit '/'
+
+    click_link 'New Project'
+    fill_in "project_name", with: "Visual studio"
+    fill_in "Description", with: "Code editing release"
+    click_button "Create Project"
+    
+    expect(page).to have_content("Project has been created")
+  end
+end
